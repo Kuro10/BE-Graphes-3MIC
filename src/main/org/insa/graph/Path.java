c5ed26d59dd2ad383a85683dd2bee69a516ae579
Kuro <trantronghieu97@gmail.com>
2018-03-23 13:17:14 +0100

    Implements Path.getTravelTime(speed) and Path.getMinimumTravelTime()

--- src/main/org/insa/graph/Path.java
+++ src/main/org/insa/graph/Path.java
@@ -224,11 +224,15 @@ public class Path {
      * @return Time (in seconds) required to travel this path at the given speed (in
      *         kilometers-per-hour).
      * 
-     * @deprecated Need to be implemented.
+     * Need to be implemented.
      */
     public double getTravelTime(double speed) {
-        // TODO:
-        return 0;
+        //done
+    	double time=0;
+    	for (Arc monArc : arcs) {
+    		time += monArc.getTravelTime(speed);
+    	}
+        return time;
     }
 
     /**
@@ -237,11 +241,15 @@ public class Path {
      * 
      * @return Minimum travel time to travel this path (in seconds).
      * 
-     * @deprecated Need to be implemented.
+     * Need to be implemented.
      */
     public double getMinimumTravelTime() {
-        // TODO:
-        return 0;
+        //done
+    	double time=0;
+    	for (Arc monArc : arcs) {
+    		time += monArc.getMinimumTravelTime();
+    	}
+        return time;
     }
 
 }
